28aa3e286e3cee01ec94f7d6bbd4db17fc4086fd
GabrielAlvsc <gabrielalves2002@gmail.com>
2024-10-30 19:16:13 -0300

    Added Score

--- Assets/Lixeira.cs
+++ Assets/Lixeira.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using TMPro;
 
 public class Lixeira : MonoBehaviour
 {
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreTextColor;
     void OnTriggerEnter(Collider other)
     {
+
+        
         if (other.gameObject.CompareTag("Papel"))
         {
+            score++;
             Debug.Log("Acertou!");
             Destroy(other.gameObject);  // Remove o papel da cena
+            scoreText.text = score.ToString();
+            scoreTextColor.text = score.ToString();
         }
     }
 }
